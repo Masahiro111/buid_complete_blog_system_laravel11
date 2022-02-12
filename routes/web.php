aab242beecbb77c25b613fc47a10aefe6852b53b
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-02-12 23:28:45 +0900

    1-4

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/post', function () {
+    return view('post');
+})->name('post');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
