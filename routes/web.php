6af1277cc3237a045f439a9d1430d5fa18b09649
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-02-13 00:12:37 +0900

    1-5

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,14 @@ Route::get('/post', function () {
     return view('post');
 })->name('post');
 
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
