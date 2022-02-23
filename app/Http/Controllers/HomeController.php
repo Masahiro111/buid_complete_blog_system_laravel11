c49e40fd80ad5aee00d26a22a1b73a3c762278bd
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-02-23 16:30:39 +0900

    4-23

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,6 +14,15 @@ class HomeController extends Controller
         $posts = Post::withCount('comments')->get();
         $recent_posts = Post::latest()->take(5)->get();
 
-        return view('home',  compact('posts', 'recent_posts'));
+        $categories = Category::withCount('posts')->orderBy('posts_count', 'desc')->take(10)->get();
+
+        $tags = Tag::latest()->take(50)->get();
+
+        return view('home',  compact(
+            'posts',
+            'recent_posts',
+            'categories',
+            'tags'
+        ));
     }
 }
