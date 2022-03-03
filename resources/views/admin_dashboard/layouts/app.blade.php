8a97d1381b50bee5333ec82a4c1f82da7e32e36f
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-03-04 01:41:32 +0900

    5-39

--- resources/views/admin_dashboard/layouts/app.blade.php
+++ resources/views/admin_dashboard/layouts/app.blade.php
@@ -6,25 +6,25 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!--favicon-->
-        <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+        <link rel="icon" href="{{ asset('admin_dashboard_assets/images/favicon-32x32.png') }}" type="image/png" />
         <!--plugins-->
         @yield("style")
-        <link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
-        <link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
-        <link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+        <link href="{{ asset('admin_dashboard_assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
+        <link href="{{ asset('admin_dashboard_assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
+        <link href="{{ asset('admin_dashboard_assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
         <!-- loader-->
-        <link href="assets/css/pace.min.css" rel="stylesheet" />
-        <script src="assets/js/pace.min.js"></script>
+        <link href="{{ asset('admin_dashboard_assets/css/pace.min.css') }}" rel="stylesheet" />
+        <script src="{{ asset('admin_dashboard_assets/js/pace.min.js') }}"></script>
         <!-- Bootstrap CSS -->
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="{{ asset('admin_dashboard_assets/css/bootstrap.min.css') }}" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-        <link href="assets/css/app.css" rel="stylesheet">
-        <link href="assets/css/icons.css" rel="stylesheet">
+        <link href="{{ asset('admin_dashboard_assets/css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('admin_dashboard_assets/css/icons.css') }}" rel="stylesheet">
 
         <!-- Theme Style CSS -->
-        <link rel="stylesheet" href="assets/css/dark-theme.css" />
-        <link rel="stylesheet" href="assets/css/semi-dark.css" />
-        <link rel="stylesheet" href="assets/css/header-colors.css" />
+        <link rel="stylesheet" href="{{ asset('admin_dashboard_assets/css/dark-theme.css') }}" />
+        <link rel="stylesheet" href="{{ asset('admin_dashboard_assets/css/semi-dark.css') }}" />
+        <link rel="stylesheet" href="{{ asset('admin_dashboard_assets/css/header-colors.css') }}" />
         <title>Rocker - Bootstrap 5 Admin Dashboard Template</title>
     </head>
 
@@ -32,10 +32,10 @@
         <!--wrapper-->
         <div class="wrapper">
             <!--start header -->
-            @include("layouts.header")
+            @include("admin_dashboard.layouts.header")
             <!--end header -->
             <!--navigation-->
-            @include("layouts.nav")
+            @include("admin_dashboard.layouts.nav")
             <!--end navigation-->
             <!--start page wrapper -->
             @yield("wrapper")
@@ -147,14 +147,14 @@
         </div>
         <!--end switcher-->
         <!-- Bootstrap JS -->
-        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="{{ asset('admin_dashboard_assets/js/bootstrap.bundle.min.js') }}"></script>
         <!--plugins-->
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
-        <script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
-        <script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+        <script src="{{ asset('admin_dashboard_assets/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('admin_dashboard_assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+        <script src="{{ asset('admin_dashboard_assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+        <script src="{{ asset('admin_dashboard_assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
         <!--app JS-->
-        <script src="assets/js/app.js"></script>
+        <script src="{{ asset('admin_dashboard_assets/js/app.js') }}"></script>
         @yield("script")
     </body>
 
